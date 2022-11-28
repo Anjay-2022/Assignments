5d9bc3e9c6bfe364b9eff054b093a19d90c95134
Anjay-2022 <anjay2013@gmail.com>
2022-11-28 10:28:52 +0530

    second commit

--- JunitTesting.java
+++ JunitTesting.java
@@ -1,9 +1,11 @@
 package day20;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class JunitTesting {
+	
 	@Test
 	public void happyCase() {
 		User obj = new User();
@@ -12,16 +14,35 @@ public class JunitTesting {
 		assertTrue("HAPPY", obj.checkemail("anjay2013@gmail.com"));
 		assertTrue("HAPPY", obj.checkpassword("Anjay@12"));
 		assertTrue("HAPPY", obj.checkmobileno("+91 7053211617"));
-
 	}
 
 	@Test
-	public void sadCase() {
+	public void firstNameSadCase() {
 		User obj = new User();
-		assertTrue("HAPPY", obj.checkFirstName("anjay"));
-		assertTrue("HAPPY", obj.checklastName("de"));
-		assertTrue("HAPPY", obj.checkemail("anjay2013@gmail.com1"));
-		assertTrue("HAPPY", obj.checkpassword("An@12"));
-		assertTrue("HAPPY", obj.checkmobileno("+91 -7053211617"));
+		assertFalse("SAD",obj.checkFirstName("anjay"));
+	}
+	
+	@Test
+	public void lastNameSadCase() {
+		User obj = new User();
+		assertFalse("SAD case", obj.checklastName("de"));
+	}
+	
+	@Test
+	public void emailSadCase() {
+		User obj = new User();
+		assertFalse("SAD", obj.checkemail("anjay@@gmail.com1"));
+	}
+	
+	@Test
+	public void passwordSadCase() {
+		User obj = new User();
+		assertFalse("SAD", obj.checkpassword("An@12"));
+	}
+	
+	@Test
+	public void mobileSadCase() {
+		User obj = new User();
+		assertFalse("SAD", obj.checkmobileno("+91 -7053211617"));
 	}
 }
